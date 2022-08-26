032a92ab6304f070382173225f7e50b880b85cfa
Keren Moreno <89100987+KerenMoreno16@users.noreply.github.com>
2022-08-25 21:13:28 -0300

    Class 3_1

--- app_flutter/app_01/lib/main.dart
+++ app_flutter/app_01/lib/main.dart
@@ -41,3 +41,24 @@ class HomePage extends StatelessWidget {
       );
   }
 }
+
+class HomePage1 extends StatefulWidget {
+  const HomePage1({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage1> createState() => _HomePage1State();
+}
+
+class _HomePage1State extends State<HomePage1> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Welcome to Flutter"),
+        ),
+        body: const Center(
+          child: Text("Hello World!"),
+        ),
+      );
+  }
+}
